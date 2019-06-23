743fcc5de72fec997966e907b33e959edd1c39ab
Brandon <itsbhenry@gmail.com>
2019-06-23 08:55:24 -0500

    Removed unnecessary items

--- frontend/src/app/remote-upload/remote-upload.component.ts
+++ frontend/src/app/remote-upload/remote-upload.component.ts
@@ -18,33 +18,9 @@ export class RemoteUploadComponent implements OnInit {
     });
   }
 
-  onFileChange(event) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.form.get('fileUpload').setValue(file);
-    }
-  }
-
-  onSubmit() {
-    const formData = new FormData();
-    formData.append('file', this.form.get('fileUpload').value);
-    this.beatOnApi.uploadFile(formData).subscribe(
-      (res) => {
-        console.log("File uploaded")
-      },
-      (err) => {
-        console.log("Failed to upload: " + err);
-      }
-    );
-  }
-
-  files: any = [];
-
   uploadFile(event) {
     for (let index = 0; index < event.length; index++) {
       const file = event[index];
-      console.log(file)
-      this.files.push(file.name)
       const formData = new FormData();
       formData.append('file', file);
       this.beatOnApi.uploadFile(formData).subscribe(
@@ -55,8 +31,5 @@ export class RemoteUploadComponent implements OnInit {
           console.log("Failed to upload: " + err);
         });
     }
-  }
-  deleteAttachment(index) {
-    this.files.splice(index, 1)
   }
 }
